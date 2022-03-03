c76415bbe44826b8b9acaa297d77c2ee9327f3e8
VlV <92279938+VlV-515@users.noreply.github.com>
2022-03-03 16:12:05 -0600

    refactor🧩:Add id in interfaces and data mock

--- src/app/interfaces/item-shop.interface.ts
+++ src/app/interfaces/item-shop.interface.ts
@@ -1,9 +1,11 @@
 export interface ItemShopInterface {
+  id: number;
   tittle: string;
   price: number;
   stock: number;
 }
 export interface ItemCartInterface {
+  id: number;
   tittle: string;
   price: number;
   units: number;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,19 +2,22 @@ export const environment = {
   production: true,
   dataMock: [
     {
+      id: 1,
       tittle: 'Producto 1',
       price: 10,
       stock: 10,
     },
     {
+      id: 2,
       tittle: 'Producto 2',
       price: 20,
       stock: 20,
     },
     {
+      id: 3,
       tittle: 'Producto 3',
       price: 30,
       stock: 30,
     },
   ],
-};
+  };
