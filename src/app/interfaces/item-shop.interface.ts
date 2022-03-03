0a210ed1418dacd0c2982d3d54c924ec819073e2
VlV <92279938+VlV-515@users.noreply.github.com>
2022-03-03 13:48:52 -0600

    feat⚙️:Add item interface

---
+++ src/app/interfaces/item-shop.interface.ts
@@ -0,0 +1,11 @@
+export interface ItemShopInterface {
+  tittle: string;
+  price: number;
+  stock: number;
+}
+export interface ItemCartInterface {
+  tittle: string;
+  price: number;
+  units: number;
+  subtotal: number;
+}
