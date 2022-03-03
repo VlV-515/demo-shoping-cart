23d57ed03098fba5231cdd635272f9ee869bd7d1
VlV <92279938+VlV-515@users.noreply.github.com>
2022-03-03 13:48:00 -0600

    feat⚙️:Add data mock

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  dataMock: [
+    {
+      tittle: 'Producto 1',
+      price: 10,
+      stock: 10,
+    },
+    {
+      tittle: 'Producto 2',
+      price: 20,
+      stock: 20,
+    },
+    {
+      tittle: 'Producto 3',
+      price: 30,
+      stock: 30,
+    },
+  ],
 };
